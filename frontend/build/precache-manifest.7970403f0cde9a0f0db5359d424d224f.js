9b50befb73968d40e4939d1fbc1366f5fe27b9e9
Chase Caster <sealgair@gmail.com>
2022-08-02 17:21:51 -0500

    npm build

---
+++ frontend/build/precache-manifest.7970403f0cde9a0f0db5359d424d224f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0bf377f4ae6c539e659",
+    "url": "/static/css/main.c606693e.chunk.css"
+  },
+  {
+    "revision": "b0bf377f4ae6c539e659",
+    "url": "/static/js/main.b0bf377f.chunk.js"
+  },
+  {
+    "revision": "d6ad0681366502a3b3ea",
+    "url": "/static/js/1.d6ad0681.chunk.js"
+  },
+  {
+    "revision": "75c4763d8a4deef9d3a3",
+    "url": "/static/js/2.75c4763d.chunk.js"
+  },
+  {
+    "revision": "1d5addb8a819dfdd6347",
+    "url": "/static/js/runtime~main.1d5addb8.js"
+  },
+  {
+    "revision": "51c934afb61ee321ae0d9f05a4b432e9",
+    "url": "/static/media/getFetch.51c934af.cjs"
+  },
+  {
+    "revision": "90f8504e50b5999ea6da0f39bba82294",
+    "url": "/index.html"
+  }
+];
